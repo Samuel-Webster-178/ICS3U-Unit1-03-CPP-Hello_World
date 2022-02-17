fbd156f845f5f2ac9611e784e99ed6ed765a3667
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-02-17 19:44:10 +0000

    innitial commit

---
+++ properHelloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Samuel Webster All rights reserved
+//
+// Created by Samuel Webster
+// Created on February 2022
+// This is a "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
